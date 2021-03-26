80a75bc9dcabdad2e0c862f7400fe7e9379b3b08
Brian Downs <brian.downs@gmail.com>
2021-03-26 10:14:34 -0700

    more
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ BINDIR = bin
 BINARY = $(shell basename $(shell pwd))
 
 $(BINDIR)/$(BINARY): clean $(BINDIR)
-	$(CC) -o $@ *.c -O3
+	$(CC) -o $@ *.c -O3 -finstrument-functions
 
 $(BINDIR):
 	mkdir -p $@
--- main.c
+++ main.c
@@ -2,9 +2,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "spinner.h"
+
+#ifdef GLIMPSE_ENABLED 
+#define GLIMPSE_FN_DUR()                            \
+    clock_t t;                                      \
+    t = clock();                                    \
+    sleep(5);                                       \
+    t = clock() - t;                                \
+    double time_taken = ((double)t)/(CLOCKS_PER_SEC/1000); \
+    printf("%s took %f milliseconds to execute \n", __func__, time_taken);
+#endif
+
+void
+run() 
+{
+    GLIMPSE_FN_DUR();
+}
 
 int
 main(int argc, char** argv)
@@ -12,7 +29,8 @@ main(int argc, char** argv)
     spinner_t* s = spinner_new(9);
     s->delay = 100000;
     s->prefix = "Doing all the things... ";
-    s->final_msg = "\nComplete!\n";
+    run();
+    s->final_msg = "Complete!\n";
     spinner_start(s);
     sleep(60);
     spinner_stop(s);
